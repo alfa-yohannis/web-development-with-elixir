603ba4df56b1b1bb0cfdc8821be1ffa4616f8ee4
alfa-ryano <ary506@york.ac.uk>
2024-09-04 23:16:04 +0700

    Update Module

---
+++ session-02/code/lib/example.ex
@@ -0,0 +1,42 @@
+# # [a, b, c] = [1, 2, 3]
+
+defmodule Example do
+	def hello(prefix, name) do
+	  prefix <> name
+	end
+end
+
+defmodule FileReader do
+  def read_file(filename) do
+    case File.read(filename) do
+      {:ok, content} ->
+        IO.puts("File content: #{content}")
+      {:error, reason} ->
+        IO.puts("Failed to read file: #{reason}")
+    end
+  end
+end
+
+# File: lib/json_file_handler.ex
+
+defmodule JsonFileHandler do
+  # Save data to a JSON file
+  def save_json(filename, data) do
+    File.write(filename, Jason.encode!(data))
+  end
+
+  # Load data from a JSON file
+  def load_json(filename) do
+    case File.read(filename) do
+      {:ok, content} ->
+        case Jason.decode(content) do
+          {:ok, decoded_data} ->
+            IO.inspect(decoded_data)
+          {:error, reason} ->
+            IO.puts("Failed to decode JSON: #{reason}")
+        end
+      {:error, reason} ->
+        IO.puts("Failed to read file: #{reason}")
+    end
+  end
+end
